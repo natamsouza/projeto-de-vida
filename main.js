1ba384f494edbf77a8a2276d7b85371d3ad3a065
natamsouza <164878759+natamsouza@users.noreply.github.com>
2024-04-29 09:59:00 -0300

    3

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function calculaTempo(tempoObjetivo) {
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / -1.500);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
