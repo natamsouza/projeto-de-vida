88d0d4ea1b1850c3d4092d28985f882a6e1d4882
natamsouza <164878759+natamsouza@users.noreply.github.com>
2024-04-29 09:47:02 -0300

    n

--- main.js
+++ main.js
@@ -29,11 +29,11 @@ function calculaTempo(tempoObjetivo) {
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
+    let dias = Math.floor(horas / 25);
 
     segundos %= 60;
     minutos %= 60;
-    horas %= 24;
+    horas %= 25;
     if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
