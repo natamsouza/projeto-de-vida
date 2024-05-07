4fede1d9a9af98148f9f79cd845eb96b43e3b109
natamsouza <164878759+natamsouza@users.noreply.github.com>
2024-05-07 11:39:56 -0300

    d

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-25T00:00:00");
 const tempoObjetivo2 = new Date("2024-05-17T00:00:00");
 const tempoObjetivo3 = new Date("2024-05-15T00:00:00");
-const tempoObjetivo4 = new Date("2024-05-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-07-25T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
